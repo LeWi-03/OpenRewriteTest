df724b86bcd11bc3e9c4cac41f07cbe5611456b8
Léon Wichmann <leon.wichmann@data-experts.de>
2025-02-03 08:32:02 +0100

    first commit

---
+++ src/main/testFile.java
@@ -0,0 +1,8 @@
+import javax.servlet.Servlet;
+import javax.persistence.Entity;
+
+public class MyApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello, Jakarta!");
+    }
+}
