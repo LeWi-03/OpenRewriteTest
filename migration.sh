ccfe21aaebf66f4fe92daf29e047dc2adda73206
Léon Wichmann <leon.wichmann@data-experts.de>
2025-02-03 08:38:39 +0100

    changed project name

--- migration.sh
+++ migration.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Durchläuft das Projektverzeichnis
-for project in "d:/OpenRewritePlayground/src/main/"; do
+for project in "d:/OpenRewriteTest/src/main/"; do
   # Überprüft, ob das Verzeichnis existiert
   if [ -d "$project" ]; then
     echo "Verzeichnis gefunden: $project"  # Gibt den Verzeichnisnamen aus
     cd "$project" || exit
 
     echo "Führe mod run aus für: $project"
-    mod run --recipe org.openrewrite.java.migrate.jakarta.JavaxMigrationToJakarta --repository-path = d:/OpenRewritePlayground/src/main/
+    mod run --recipe org.openrewrite.java.migrate.jakarta.JavaxMigrationToJakarta --repository-path = d:/OpenRewriteTest/src/main/
   else
     echo "Verzeichnis $project nicht gefunden!"
   fi
